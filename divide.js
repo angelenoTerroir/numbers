7bfb31bb6af32acf1301807ae1aecd09083e591a
jeffsmithink <writejeffsmith@gmail.com>
2018-03-11 00:02:43 -0800

    init

---
+++ divide.js
@@ -0,0 +1,14 @@
+function isDivisible(divisee, divisor) {
+   return divisee % divisor === 0;
+}
+
+function testIsDivisible() {
+  if (isDivisible(10, 2) && !isDivisible(11, 2)) {
+    console.log('SUCCESS: `isDivisible` is working');
+  }
+  else {
+    console.log('FAILURE: `isDivisible` is not working');
+  }
+}
+
+testIsDivisible();
---
+++ rectangle.js
@@ -0,0 +1,17 @@
+function computeArea(width, height) {
+  return width * height;
+}
+
+function testComputeArea() {
+  var width = 3;
+  var height = 4;
+  var expected = 12;
+  if (computeArea(width, height) === expected) {
+    console.log('SUCCESS: `computeArea` is working');
+  }
+  else {
+    console.log('FAILURE: `computeArea` is not working');
+  }
+}
+
+testComputeArea();
